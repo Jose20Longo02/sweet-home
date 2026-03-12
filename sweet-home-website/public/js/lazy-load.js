4c02eba980887bb23b6f69bea2c7432d65d5963e
Jose20Longo02 <JoseLongo@Medialy.Agency>
2026-03-11 20:42:10 -0600

    Update lazy-load.js

--- sweet-home-website/public/js/lazy-load.js
+++ sweet-home-website/public/js/lazy-load.js
@@ -1,11 +1,10 @@
 // Lazy-load non-critical scripts when idle
+// Note: home.js is loaded directly by home.ejs; do not add it here or it loads twice and throws "Identifier 'HOME_I18N' has already been declared"
 (function() {
   'use strict';
   if (window.requestIdleCallback) {
     window.requestIdleCallback(function(){
-      [
-        '/js/home.js'
-      ].forEach(function(src){
+      [].forEach(function(src){
         var s = document.createElement('script');
         s.src = src;
         s.defer = true;
